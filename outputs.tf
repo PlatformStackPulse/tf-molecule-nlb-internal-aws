1da1f5c5217816d23c250ac426750fc7a8cff4ac
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:29:25 +0100

    feat: implement tf-molecule-nlb-internal-aws molecule

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "nlb_arn" {
+  description = "ARN of the NLB"
+  value       = module.nlb.arn
+}
+
+output "nlb_dns_name" {
+  description = "DNS name of the NLB"
+  value       = module.nlb.dns_name
+}
+
+output "nlb_zone_id" {
+  description = "Route53 zone ID of the NLB"
+  value       = module.nlb.zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = module.target_group.arn
 }
