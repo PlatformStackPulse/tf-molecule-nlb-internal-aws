4b9ef37082e4fae32873e89622a8ac929a5f55d0
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:29:25 +0100

    feat: implement tf-molecule-nlb-internal-aws molecule

--- variables.tf
+++ variables.tf
@@ -1,7 +1,27 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Private subnet IDs for the NLB (min 2)"
+  type        = list(string)
+}
+
+variable "listener_port" {
+  description = "Port the NLB listens on"
+  type        = number
+  default     = 80
+}
+
+variable "target_port" {
+  description = "Port the targets listen on"
+  type        = number
+  default     = 80
+}
+
+variable "target_type" {
+  description = "Target type (instance, ip)"
+  type        = string
+  default     = "instance"
+}
